c69324574287fc002af03ff06ea7d2e46c81341b
Yaro <glodov@gmail.com>
2017-07-01 12:11:21 +0300

    Very first step.

---
+++ machine.js
@@ -0,0 +1,29 @@
+// Layers OS
+
+// machine is global variable, such window in 
+var machine = {
+  devices: {
+    input: {
+      keyboard: [],
+      mouse: [],
+      touchpad: []
+    },
+    output: {
+      monitor: []
+      printer: []
+    },
+    network: {
+      lo: [],
+      en: [],
+      p2p: [],
+      utun: []
+    }
+  },
+  display: [
+    {
+      id: "PHL276E6",
+      name: "Philips 276E",
+      colors: 16M,  
+    }
+  ]
+};
---
+++ types.js
@@ -0,0 +1,39 @@
+#!/bin/a
+
+
+// Integer type:
+// min: -2^60
+// max: +2^60
+// min Infinity: -2^61
+// max Infinity: +2^61
+
+// 1K  = 1,024
+// 1M  = 1,048,576
+// 1G  = 1,073,741,824
+// 1T  = 1,099,511,627,776
+// 1P  = 1,125,899,906,842,620
+// 1E  = 1.152 921 504 606 843 e18
+// 1Z  = 1.180 591 620 717 407 e21 out of range (64bit integer)
+// 1eX = 10 ^ X
+// 1b  = binary number: 0101001b
+// 1x  = hex number: 012Fx
+// 1B  = byte 256 (2^8) :: 1K + 4B = 2K = 2,048
+
+// byte
+// byte[]
+
+
+machine.types = {
+	Array: '',
+	Boolean: '',
+	Function: '',
+	Infinity: '',
+	Number: '',
+	Object: '',
+	String: '',
+};
+
+
+function parseType(byte[] input) {
+	Infin
+}
